2bfe9b8a7074fe7d07902c7a6b50bb651937b4ff
thejayadad <thejayadad@gmail.com>
2023-09-24 18:37:50 -0400

    update

--- myapp/app/post/[slug]/page.js
+++ myapp/app/post/[slug]/page.js
@@ -2,12 +2,39 @@
 import React from 'react';
 import Image from 'next/image';
 
-const PostPage = () => {
+const PostPage = ({ data, slug }) => {
   return (
-    <section className='mx-auto mt-12 flex flex-col md:flex-row md:items-center gap-12'>
-      <div className='flex-1'>
-      <h1 className="text-3xl md:text-xl font-extrabold text-blue-500 mb-6">Title</h1>
+    <section className='container mx-auto mt-12 flex flex-col md:flex-row md:items-center gap-12'>
+<div className="flex-1">
+        <h1 className="text-4xl md:text-6xl font-extrabold text-blue-500 mb-6">title</h1>
+        <div className="flex items-center gap-4">
+            <div className="w-12 h-12 rounded-full overflow-hidden">
+              <img src='https://images.pexels.com/photos/5643191/pexels-photo-5643191.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt="" className="w-full h-full" />
+            </div>
 
+          <div className="flex flex-col">
+            <span className="text-lg font-semibold">{data?.user.name}</span>
+            <span className="text-gray-500">01.01.2024</span>
+          </div>
+        </div>
+      </div>
+
+        <div className="w-full md:w-1/2">
+          <img
+          src='https://images.pexels.com/photos/5643191/pexels-photo-5643191.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+          alt="pic" className="w-full h-auto rounded-lg" />
+        </div>
+
+      <div className="flex-1 mt-6 md:mt-0">
+        <div className="text-xl md:text-2xl font-normal leading-relaxed mb-6">
+          <div dangerouslySetInnerHTML={{ __html: data?.desc }} />
+        </div>
+        <div className="mt-8">
+          Comments
+        </div>
+      </div>
+      <div className="flex-1 md:hidden">
+        menu
       </div>
     </section>
   )
