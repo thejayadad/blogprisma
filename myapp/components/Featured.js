bce94101a70cf6a80ee8b68fec6765dcf3f4b442
thejayadad <thejayadad@gmail.com>
2023-09-24 12:11:17 -0400

    update

--- myapp/components/Featured.js
+++ myapp/components/Featured.js
@@ -5,7 +5,25 @@ import React from 'react'
 const Featured = () => {
   return (
     <section>
-        Featured
+            <div className="relative bg-cover bg-center bg-fixed h-screen  rounded-sm" style={{ backgroundImage: "url('https://images.pexels.com/photos/2696064/pexels-photo-2696064.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')" }}>
+      <div className="absolute inset-0 bg-black opacity-40  rounded-sm"></div>
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="text-center text-white">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold leading-tight mb-4">
+            Welcome to CulinaryExperience
+          </h1>
+          <p className="text-lg sm:text-xl md:text-2xl mb-8">
+            Explore the world of culinary delights with us.
+          </p>
+          <a
+            href="#your-link"
+            className="px-6 py-3 bg-gray-400 hover:bg-gray-200 text-white text-lg font-semibold rounded-sm transition duration-300 ease-in-out transform hover:scale-105"
+          >
+            Get Started
+          </a>
+        </div>
+      </div>
+    </div>
     </section>
   )
 }
--- myapp/components/Navbar.js
+++ myapp/components/Navbar.js
@@ -1,11 +1,12 @@
 'use client'
+import Link from 'next/link';
 import React from 'react';
 import { FaInstagram, FaYoutube, FaFacebook, FaTiktok } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <header className="flex justify-between p-4 bg-primary text-gray-500 h-[100px] items-center min-w-full">
-      <div className="flex space-x-4 cursor-pointer flex-1">
+      <div className="flex space-x-1 cursor-pointer flex-1">
         <span
           className="text-pink-500 hover:text-pink-700"
         >
@@ -27,13 +28,20 @@ const Navbar = () => {
           <FaTiktok size={24} />
         </span>
       </div>
-      <div className="flex items-center flex-1">
+      <div className="hidden md:flex items-center flex-1">
       <h1 className="text-3xl font-extrabold text-blue-500">Culinary</h1>
       <h2 className="text-3xl font-semibold text-orange-500">Experience</h2>
     </div>
 
-      <div className=' flex flex-1 justify-end'>
-        Action
+      <div className=' flex flex-1 justify-end gap-2'>
+        <Link
+        className='px-2 py-1 bg-orange-500 rounded-sm text-white hover:bg-orange-700'
+        href={'/login'}
+        >Login</Link>
+                <Link
+        className='px-2 py-1 bg-blue-500 rounded-sm text-white hover:bg-blue-700'
+        href={'/register'}
+        >Register</Link>
       </div>
     </header>
   );
